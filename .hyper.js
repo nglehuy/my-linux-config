e21755a1d3f49d6b2d3b3c0fd5a3da57cbbfe117
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-05-22 22:41:40 +0700

    Update

--- .hyper.js
+++ .hyper.js
@@ -12,11 +12,11 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 13,
 
     // font family with optional fallbacks
     fontFamily:
-      'Dejavu Sans Mono, Hack, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      'Monaco Nerd Font Mono, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
@@ -25,7 +25,7 @@ module.exports = {
     fontWeightBold: 'bold',
 
     // line height as a relative unit
-    lineHeight: 1.0,
+    lineHeight: 1.2,
 
     // letter spacing as a relative unit
     letterSpacing: 0,
@@ -63,7 +63,7 @@ module.exports = {
 
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+    showHamburgerMenu: true,
 
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
@@ -150,7 +150,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-base16-tomorrow-dark', 'hypercwd', 'hyperminimal'],
+  plugins: ['hyperterm-base16-tomorrow-dark', 'hypercwd'],
   //'hyperborder',
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
--- .zshrc
+++ .zshrc
@@ -128,3 +128,18 @@ prompt spaceship
 
 # hyper
 unsetopt PROMPT_SP
+
+# hadoop
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+export PATH=${JAVA_HOME}/bin:${PATH}
+export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
+export HADOOP_HOME=/home/$USER/hadoop-3.2.0
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+
