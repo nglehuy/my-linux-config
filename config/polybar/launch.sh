ae2baa0c63ff390755926ff152f2a69088a25737
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-08-23 14:44:42 +0700

    Update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Fix login issue
+sleep 1
+
 # Launch bar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
