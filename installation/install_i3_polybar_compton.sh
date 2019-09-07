2edbdb1170e3e9679d0286bdfac0fcd092891918
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-09-07 15:29:56 +0700

    Update

--- installation/install_i3_polybar_compton.sh
+++ installation/install_i3_polybar_compton.sh
@@ -2,27 +2,29 @@
 
 echo "Add speed ricer repo ..................................."
 # Speed ricer
-sudo add-apt-repository ppa:kgilmer/speed-ricer -y
+#sudo add-apt-repository ppa:kgilmer/speed-ricer -y
 
 echo "Installing ............................................."
 sudo apt install i3-gaps polybar compton rofi dunst -y
 
 echo "Installing i3-gnome ...................................."
 mkdir ~/open-source-apps
-git clone https://github.com/i3-gnome/i3-gnome.git ~/open-source-apps/i3-gnome
+#git clone https://github.com/i3-gnome/i3-gnome.git ~/open-source-apps/i3-gnome
 cd ~/open-source-apps/i3-gnome
 sudo make install
+
+cd ~/my-linux-config
 
 echo "Hard linking config files .............................."
 mkdir ~/.config/i3
 rm -r ~/.config/i3/*
-ln ../config/i3/config ~/.config/i3/
+ln ./config/i3/config ~/.config/i3/
 mkdir ~/.config/polybar
 rm -r ~/.config/polybar/*
-ln ../config/polybar/* ~/.config/polybar/
+ln ./config/polybar/* ~/.config/polybar/
 mkdir ~/.config/compton
 rm -r ~/.config/compton/*
-ln ../config/compton/compton.conf ~/.config/compton
+ln ./config/compton/compton.conf ~/.config/compton
 mkdir ~/.config/dunst
 rm -r ~/.config/dunst/*
-ln ../config/dunst/dunstrc ~/.config/dunst/
+ln ./config/dunst/dunstrc ~/.config/dunst/
--- installation/link_configs.sh
+++ installation/link_configs.sh
@@ -3,29 +3,29 @@
 echo "Linking alacritty ......................................"
 mkdir ~/.config/alacritty
 rm -r ~/.config/alacritty/*
-ln ../config/alacritty/alacritty.yml ~/.config/alacritty/
+ln ./config/alacritty/alacritty.yml ~/.config/alacritty/
 
 echo "Linking neovim ........................................."
 mkdir ~/.config/nvim
 rm -r ~/.config/nvim/*
-ln ../config/nvim/init.vim ~/.config/nvim/
+ln ./config/nvim/init.vim ~/.config/nvim/
 
 echo "Linking hyper terminal ................................."
 rm ~/.hyper.js
-ln ../home/.hyper.js ~/
+ln ./home/.hyper.js ~/
 
 echo "Linking vim config ....................................."
 rm ~/.vimrc
-ln ../home/.vimrc ~/
+ln ./home/.vimrc ~/
 
 echo "Linking zsh config ....................................."
 rm ~/.zshrc
-ln ../home/.zshrc ~/
+ln ./home/.zshrc ~/
 
 echo "Linking colorschemes ..................................."
 mkdir ~/.config/colorschemes
 rm -r ~/.config/colorschemes/*
-ln ../config/colorschemes/* ~/.config/colorschemes/
+ln ./config/colorschemes/* ~/.config/colorschemes/
 
 echo "Executing pywal colorschemes ..........................."
 cd ~/
@@ -34,4 +34,4 @@ wal -f ~/.config/colorschemes/gruvbox.json
 echo "Linking rofi config ...................................."
 mkdir ~/.config/rofi
 rm ~/.config/rofi/*
-ln ../config/rofi/* ~/.config/rofi/
+ln ./config/rofi/* ~/.config/rofi/
