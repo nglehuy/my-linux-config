fb2ef3b3a2212860297ab26360a4f05d1a34ed0f
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-09-07 17:53:54 +0700

    Update

--- installation/install_apps.sh
+++ installation/install_apps.sh
@@ -1,85 +1,124 @@
 #!/bin/bash
 
-echo "Installing apps ..........................................................."
-sudo apt install nodejs \
-    ibus-hangul \
-    ibus-bamboo \
-    vim \
-    curl \
-    git \
-    build-essential \
-    alacritty \
-    default-jdk \
-    default-jre \
-    python-pip \
-    python3-pip \
-    zsh \
-    ubuntu-restricted-extras \
-    libsqlite3-dev \
-    pkg-config \
-    libnotify-dev \
-    fonts-noto \
-    fonts-roboto \
-    fonts-font-awesome \
-    chromium-browser \
-    vlc \
-    inotify-tools \
-    -y
+echo "Installing apps ...........................................................?"
+read yes
+if [[ $yes == y* ]]; then
+    sudo apt install nodejs \
+        ibus-hangul \
+        ibus-bamboo \
+        vim \
+        build-essential \
+        alacritty \
+        default-jdk \
+        default-jre \
+        python-pip \
+        python3-pip \
+        zsh \
+        ubuntu-restricted-extras \
+        libsqlite3-dev \
+        pkg-config \
+        libnotify-dev \
+        fonts-noto \
+        fonts-roboto \
+        fonts-font-awesome \
+        vlc \
+        inotify-tools \
+        neofetch \
+        papirus-icon-theme \
+        -y
+else
+    echo "Not installing apps"
+fi
 
-echo "Installing Linuxbrew ......................................................"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+echo "Installing Linuxbrew ......................................................?"
+read yes
+if [[ $yes == y* ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+else
+    echo "Not installing Linuxbrew"
+fi
 
-echo "Installing neovim ........................................................."
-brew install neovim
-sudo -H pip3 install neovim virtualenv virtualenvwrapper
-sudo -H pip install neovim
+echo "Installing neovim .........................................................?"
+read yes
+if [[ $yes == y* ]]; then
+    brew install neovim
+    sudo -H pip3 install neovim virtualenv virtualenvwrapper
+    sudo -H pip install neovim
+else
+    echo "Not installing neovim"
+fi
 
-echo "Installing pywal for colorscheme .........................................."
-sudo -H pip3 install pywal
+echo "Installing pywal for colorscheme ..........................................?"
+read yes
+if [[ $yes == y* ]]; then
+    sudo -H pip3 install pywal
+else
+    echo "Not installing pywal"
+fi
 
-echo "Installing nodejs modules ................................................."
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-export PATH=~/.npm-global/bin:$PATH
-npm install -g spaceship-prompt react react-cli react-native react-native-cli create-react-app
+echo "Installing nodejs modules .................................................?"
+read yes
+if [[ $yes == y* ]]; then
+    mkdir ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    export PATH=~/.npm-global/bin:$PATH
+    npm install -g spaceship-prompt react react-cli react-native react-native-cli create-react-app
+else
+    echo "Not installing nodejs modules"
+fi
 
-echo "Cloning open souce apps ..................................................."
-mkdir ~/open-source-apps
-
-# Onedrive
-echo "Setting up onedrive ......................................................."
-curl -fsS https://dlang.org/install.sh | bash -s dmd
-source ~/dlang/dmd-2.081.1/activate
-echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-git clone https://github.com/abraunegg/onedrive.git ~/open-source-apps/onedrive-installation
-cd ~/open-source-apps/onedrive-installation
-./configure
-make clean; make;
-sudo make install
-
-# Powerline fonts
-echo "Setting up powerline fonts ................................................"
-git clone https://github.com/powerline/fonts.git --depth=1 ~/open-source-apps/powerline-fonts
-cd ~/open-source-apps/powerline-fonts
-./install.sh
+echo "Cloning open souce apps ...................................................?"
+read yes
+if [[ $yes == y* ]]; then
+    mkdir ~/open-source-apps
+    
+    # Onedrive
+    echo "Setting up onedrive .......................................................?"
+    curl -fsS https://dlang.org/install.sh | bash -s dmd
+    source ~/dlang/dmd-2.081.1/activate
+    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+    git clone https://github.com/abraunegg/onedrive.git ~/open-source-apps/onedrive-installation
+    cd ~/open-source-apps/onedrive-installation
+    ./configure
+    make clean; make;
+    sudo make install
+    
+    # Powerline fonts
+    echo "Setting up powerline fonts ................................................?"
+    git clone https://github.com/powerline/fonts.git --depth=1 ~/open-source-apps/powerline-fonts
+    cd ~/open-source-apps/powerline-fonts
+    ./install.sh
+else
+    echo "Not cloning open source apps"
+fi
 
 # Vim Plugin Managers
-echo "Installing vim plugin managers ............................................"
-cd ~/
-# Vim Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Installing vim plugin managers ............................................?"
+read yes
+if [[ $yes == y* ]]; then
+    cd ~/
+    # Vim Plug
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # Vundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    # Pathogen
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+else
+    echo "Not installing vim plugins"
+fi
 
 # Zsh Plugins
-echo "Installing zsh plugins ...................................................."
-cd ~/
-# Oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Zsh autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-# Zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo "Installing zsh plugins ....................................................?"
+read yes
+if [[ $yes == y* ]]; then
+    cd ~/
+    # Oh my zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # Zsh autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+    # Zsh syntax highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+else
+    echo "Not installing zsh plugins"
+fi
--- installation/install_i3_polybar_compton.sh
+++ installation/install_i3_polybar_compton.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 
-echo "Add speed ricer repo ..................................."
-# Speed ricer
-#sudo add-apt-repository ppa:kgilmer/speed-ricer -y
+echo "Add speed ricer repo ...................................?"
+read yes
+if [[ $yes == y* ]]; then
+    # Speed ricer
+    sudo add-apt-repository ppa:kgilmer/speed-ricer -y
+else
+    echo "Not adding repo"
+fi
 
-echo "Installing ............................................."
-sudo apt install i3-gaps polybar compton rofi dunst -y
+echo "Installing .............................................?"
+read yes
+if [[ $yes == y* ]]; then
+    sudo apt install i3-gaps gnome-flashback polybar feh rofi dunst -y
+else
+    echo "Not installing apps"
+fi
 
-echo "Installing i3-gnome ...................................."
-mkdir ~/open-source-apps
-#git clone https://github.com/i3-gnome/i3-gnome.git ~/open-source-apps/i3-gnome
-cd ~/open-source-apps/i3-gnome
-sudo make install
+echo "Installing i3-gnome ....................................?"
+read yes
+if [[ $yes == y* ]]; then
+    mkdir ~/open-source-apps
+    git clone https://github.com/i3-gnome/i3-gnome.git ~/open-source-apps/i3-gnome
+    cd ~/open-source-apps/i3-gnome
+    sudo make install
+else
+    echo "Not installing i3-gnome"
+fi
 
-cd ~/my-linux-config
-
-echo "Hard linking config files .............................."
-mkdir ~/.config/i3
-rm -r ~/.config/i3/*
-ln ./config/i3/config ~/.config/i3/
-mkdir ~/.config/polybar
-rm -r ~/.config/polybar/*
-ln ./config/polybar/* ~/.config/polybar/
-mkdir ~/.config/compton
-rm -r ~/.config/compton/*
-ln ./config/compton/compton.conf ~/.config/compton
-mkdir ~/.config/dunst
-rm -r ~/.config/dunst/*
-ln ./config/dunst/dunstrc ~/.config/dunst/
+echo "Hard linking config files ..............................?"
+read yes
+if [[ $yes == y* ]]; then
+    cd ~/my-linux-config
+    mkdir ~/.config/i3
+    rm -r ~/.config/i3/*
+    ln ./config/i3/config ~/.config/i3/
+    mkdir ~/.config/polybar
+    rm -r ~/.config/polybar/*
+    ln ./config/polybar/* ~/.config/polybar/
+    mkdir ~/.config/compton
+    rm -r ~/.config/compton/*
+    ln ./config/compton/compton.conf ~/.config/compton
+    mkdir ~/.config/dunst
+    rm -r ~/.config/dunst/*
+    ln ./config/dunst/dunstrc ~/.config/dunst/
+else
+    echo "Not hard linking"
+fi
