fb3da74f22ab29d948eb16fcee0a9bd049e2e0eb
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2019-11-27 00:44:55 +0700

    Update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -13,14 +13,16 @@ sleep 1
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m
-    polybar -c $HOME/.config/polybar/config -r right 2> /tmp/polybar_log &
-    polybar -c $HOME/.config/polybar/config -r center 2> /tmp/polybar_log &
-    polybar -c $HOME/.config/polybar/config -r left 2> /tmp/polybar_log &
+#    polybar -c $HOME/.config/polybar/config -r right 2> /tmp/polybar_log &
+#    polybar -c $HOME/.config/polybar/config -r center 2> /tmp/polybar_log &
+#    polybar -c $HOME/.config/polybar/config -r left 2> /tmp/polybar_log &
+    polybar -c $HOME/.config/polybar/config -r one 2> /tmp/polybar_log &
   done
 else
-  polybar -c $HOME/.config/polybar/config -r right 2> /tmp/polybar_log &
-  polybar -c $HOME/.config/polybar/config -r center 2> /tmp/polybar_log &
-  polybar -c $HOME/.config/polybar/config -r left 2> /tmp/polybar_log &
+#  polybar -c $HOME/.config/polybar/config -r right 2> /tmp/polybar_log &
+#  polybar -c $HOME/.config/polybar/config -r center 2> /tmp/polybar_log &
+#  polybar -c $HOME/.config/polybar/config -r left 2> /tmp/polybar_log &
+  polybar -c $HOME/.config/polybar/config -r one 2> /tmp/polybar_log &
 fi
 
 echo "Bars launched..."
