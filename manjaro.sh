691c141ad1446e25db70b97552ce5a7b170b2480
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-02-11 18:28:16 +0700

    Update

--- manjaro.sh
+++ manjaro.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-sudo pacman -Syu vim neovim git curl make cmake gcc noto-fonts noto-fonts-emoji ttf-roboto nodejs npm yarn jdk11-openjdk python-pip kitty flatpak
+sudo pacman -Syu vim neovim git curl make cmake gcc noto-fonts noto-fonts-emoji ttf-roboto nodejs npm yarn jdk11-openjdk python-pip kitty flatpak tk
